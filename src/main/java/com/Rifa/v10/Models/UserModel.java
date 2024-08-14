cd83affbade7690a1af5563b2f47656b0d3ed008
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-14 15:24:44 -0300

    a

---
+++ src/main/java/com/Rifa/v10/Models/UserModel.java
@@ -0,0 +1,28 @@
+package com.Rifa.v10.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String lastName;
+    private String cpf;
+    private String phone;
+
+    private String email;
+    private String password;
+    private boolean isAdmin = false;
+}
