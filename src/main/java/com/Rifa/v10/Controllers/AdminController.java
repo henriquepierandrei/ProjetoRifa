9eb424efb7ced8ca2b8530a2f411d702ab090e1f
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-08-14 20:40:22 -0300

    update

--- src/main/java/com/Rifa/v10/Controllers/AdminController.java
+++ src/main/java/com/Rifa/v10/Controllers/AdminController.java
@@ -1,6 +1,7 @@
 package com.Rifa.v10.Controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
+    @GetMapping
 }
